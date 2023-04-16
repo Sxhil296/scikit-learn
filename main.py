c1a116258e361a71c5f9c6f25f12ef27b9ef1fd5
Sahil <leviack629@gmail.com>
2023-04-16 23:26:08 +0530

    plotting

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x = [i for i in range(10)]
+print(x)
+y = [2*i for i in range(10)]
+print(y)
+plt.xlabel('x-axis')
+plt.ylabel('y-axis')
+# plt.plot(x, y)
+plt.scatter(x, y)
+plt.show()
